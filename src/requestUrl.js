f13165f6d9b8ca34ef839b71edaf0c45ac975b2f
João Fernando Corsini <jf.corsini@gmail.com>
2018-04-21 13:20:07 +0300

    Implements requestUrl function
    The function to calculate the time the request took was implemented. Here I also added 4 possible status, which is:
     - success, if the request status code is between 200 and 300 and the html returned contains the content required;
     - not_fulfiled, if the status code is between 200 and 300 but the html returned does not contains the content required;
     - bad_request, if the status code is not between 200 and 300;
     - server_error, if there was some problem while sending the request, e.g. wrong url.
    
    Currently this is simply logging on the same place as debugging, but it will be changed.

--- src/requestUrl.js
+++ src/requestUrl.js
@@ -1,15 +1,39 @@
 'use strict';
 
-module.exports = (event, context, callback) => {
-  console.debug('url: ', event.url, 'content', event.content);
+const STATUS_SUCCESS = 'success';
+const STATUS_NOT_FULFILLED = 'not_fulfilled';
+const STATUS_BAD_REQUEST = 'bad_request';
+const STATUS_SERVER_ERROR = 'server_error';
 
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'RequestUrl function was executed!',
-      input: event,
-    }),
-  };
+const axios = require('axios');
+const { getMilisecondsDiffFromNow } = require('../lib');
 
-  callback(null, response);
+module.exports = (event, context) => {
+  const { url, content } = event;
+
+  const time = process.hrtime();
+  axios.get(url)
+    .then((response) => {
+      const totalTime = getMilisecondsDiffFromNow(time);
+      let status = STATUS_NOT_FULFILLED;
+
+      if (response.status < 200 || response.status >= 300) {
+        status = STATUS_BAD_REQUEST;
+      } else if (response.data.indexOf(content) >= 0) {
+        status = STATUS_SUCCESS;
+      }
+
+      console.log({
+        url,
+        status,
+        totalTime,
+      });
+    })
+    .catch(() => {
+      console.log({
+        url,
+        status: STATUS_SERVER_ERROR,
+        totalTime: getMilisecondsDiffFromNow(time),
+      });
+    });
 };
